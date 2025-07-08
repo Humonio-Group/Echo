22f80c1370d0eac5b20990182d9b45c652cf4191
loicmaes <production@maesloic.fr>
2025-07-08 23:41:58 +0200

    :heavy_plus_sign: add vitest for unit testing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
-export default withNuxt({
-  files: ["**/*.ts", "**/*.vue"],
-  rules: {},
-});
+export default withNuxt([
+  {
+    files: ["**/*.vue", "**/*.ts"],
+    rules: {},
+  },
+  {
+    files: ["**/*.test.ts"],
+    rules: {
+      "import/first": "off",
+      "no-explicit-any": "off",
+    },
+  },
+]);
