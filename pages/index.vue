86dac3b2f714cef7ea0f85f62472213d17b62368
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-07-08 17:53:38 +0200

    :rocket: handle default button interaction

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-
+import { toast } from "vue-sonner";
 </script>
 
 <template>
-  <main data-page="home">
-    main app
+  <main
+    data-page="home"
+    class="min-h-dvh flex flex-col gap-4 items-center justify-center"
+  >
+    <Button @click="toast.info('Bienvenue sur Echo !')">
+      {{ $t("hello-world") }}
+    </Button>
   </main>
 </template>
