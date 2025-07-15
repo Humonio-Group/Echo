e404df571cbe6ed577c322c60abb133c4ceb7f19
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-07-15 16:03:59 +0200

    :rocket: add output management function to errors service

--- server/services/globals/errors.ts
+++ server/services/globals/errors.ts
@@ -1,5 +1,11 @@
-import type { HttpEvent } from "~/types/globals/http";
+import type { HttpEvent, StatusCode } from "~/types/globals/http";
 import type { EchoError } from "~/types/globals/errors";
+
+export function setOutput(event: HttpEvent, code: StatusCode, message: string = "") {
+  event.node.res.statusCode = code;
+  event.node.res.statusMessage = message;
+  return event;
+}
 
 export function catchError(event: HttpEvent, error: EchoError) {
   event.node.res.statusCode = error.code;
