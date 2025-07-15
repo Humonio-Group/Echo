b631af56d5fadb5236a83c2d87c6911a6ab0250e
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-07-15 18:00:56 +0200

    :rocket: bind workspaces store to workspaces selection page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { Plus } from "lucide-vue-next";
-import type { IWorkspace } from "~/types/workspaces";
+import { LoaderCircle, Plus } from "lucide-vue-next";
 import WorkspaceCard from "~/components/shared/workspaces/WorkspaceCard.vue";
 import CreateWorkspaceDialog from "~/components/shared/workspaces/dialogs/CreateWorkspaceDialog.vue";
 
@@ -8,27 +7,14 @@ definePageMeta({
   layout: "auth",
 });
 
-const workspaces: IWorkspace[] = [
-  /* {
-    id: 1,
-    name: "Humonio",
-    description: null,
-    picture: null,
-    companyInfo: "Humonio",
-    productOrService: "Humonio",
-    values: "Humonio",
-    ownerId: "loicmaes",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    members: [],
-  }, */
-];
+const store = useWorkspaceStore();
+const { workspaces, loading } = storeToRefs(store);
 </script>
 
 <template>
   <main
     data-page="home"
-    class="w-full max-w-lg min-h-dvh flex flex-col gap-4"
+    class="w-full max-w-lg min-h-dvh sm:min-h-auto flex flex-col gap-4"
   >
     <header class="flex items-center justify-between py-4">
       <p class="font-bold bg-gradient-to-r from-primary to-primary-gradient text-transparent bg-clip-text">
@@ -38,22 +24,31 @@ const workspaces: IWorkspace[] = [
       <UserButton />
     </header>
 
-    <main
-
-      class="flex-1 flex flex-col gap-3"
-    >
-      <template v-if="workspaces.length">
+    <main class="flex-1 sm:flex-auto flex flex-col gap-3">
+      <template v-if="workspaces?.length">
         <WorkspaceCard
           v-for="workspace in workspaces"
           :key="workspace.id"
           :workspace="workspace"
+        />
+
+        <LoaderCircle
+          v-if="loading.workspaces"
+          class="animate-spin self-center"
         />
       </template>
       <div
         v-else
         class="my-auto flex flex-col items-center gap-4"
       >
-        <p class="text-center text-muted-foreground">
+        <LoaderCircle
+          v-if="loading.workspaces"
+          class="animate-spin"
+        />
+        <p
+          v-else
+          class="text-center text-muted-foreground"
+        >
           {{ $t("labels.empty.workspaces") }}
         </p>
 
@@ -67,8 +62,8 @@ const workspaces: IWorkspace[] = [
     </main>
 
     <footer
-      v-if="workspaces.length"
-      class="flex flex-col"
+      v-if="workspaces?.length"
+      class="flex flex-col sm:items-center py-4"
     >
       <CreateWorkspaceDialog>
         <Button>
