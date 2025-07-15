d9b1d0c0e6afd42dd15c802271772855a87e55da
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-07-16 00:33:01 +0200

    :rocket: update workspace specific home page

---
+++ pages/[workspaceId]/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const store = useWorkspaceStore();
+const { workspace } = storeToRefs(store);
+</script>
+
+<template>
+  <main data-page="workspace.home">
+    HOME {{ workspace?.name }}
+  </main>
+</template>
