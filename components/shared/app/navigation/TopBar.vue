d104cd6c3dc442a2f75f0cbe0b4beb12e5a64707
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-07-16 10:25:14 +0200

    :rocket: add default layout navigation

---
+++ components/shared/app/navigation/TopBar.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { Home, Plus, Settings } from "lucide-vue-next";
+import BrandName from "~/components/shared/statuses/BrandName.vue";
+</script>
+
+<template>
+  <div class="flex items-center justify-between md:justify-start gap-4 md:gap-12">
+    <BrandName />
+
+    <nav class="flex-1 items-center gap-1 hidden md:flex">
+      <Button
+        size="icon"
+        variant="ghost"
+        as-child
+      >
+        <NuxtLinkLocale
+          :to="useWorkspacePath('/')"
+          active-class="!bg-accent !text-accent-foreground"
+        >
+          <Home />
+        </NuxtLinkLocale>
+      </Button>
+      <Button
+        variant="ghost"
+        as-child
+      >
+        <NuxtLinkLocale
+          :to="useWorkspacePath('/training')"
+          active-class="!bg-accent !text-accent-foreground"
+        >
+          {{ $t("navigation.training") }}
+        </NuxtLinkLocale>
+      </Button>
+      <Button
+        variant="ghost"
+        as-child
+      >
+        <NuxtLinkLocale
+          :to="useWorkspacePath('/performance')"
+          active-class="!bg-accent !text-accent-foreground"
+        >
+          {{ $t("navigation.performance") }}
+        </NuxtLinkLocale>
+      </Button>
+      <Button
+        variant="ghost"
+        as-child
+      >
+        <NuxtLinkLocale
+          :to="useWorkspacePath('/team')"
+          active-class="!bg-accent !text-accent-foreground"
+        >
+          {{ $t("navigation.team") }}
+        </NuxtLinkLocale>
+      </Button>
+    </nav>
+
+    <div class="flex items-center gap-2">
+      <Button
+        size="icon"
+        variant="ghost"
+      >
+        <Settings />
+      </Button>
+      <Button
+        size="icon"
+        class="hidden md:flex mr-4"
+      >
+        <Plus />
+      </Button>
+      <UserButton />
+    </div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,40 @@
 <script setup lang="ts">
-import BrandName from "~/components/shared/statuses/BrandName.vue";
+// TODO: import { Plus } from "lucide-vue-next";
+import MobileBar from "~/components/shared/app/navigation/MobileBar.vue";
+import TopBar from "~/components/shared/app/navigation/TopBar.vue";
+
+const onMobile = isMobile();
 </script>
 
 <template>
-  <div data-layout="default">
-    <header class="flex items-center py-4 px-6 justify-between border-b">
-      <BrandName />
-
-      <UserButton />
+  <div
+    data-layout="default"
+    class="flex flex-col"
+  >
+    <header class="sticky top-0 py-3 px-4 bg-background">
+      <TopBar />
     </header>
-    <main class="py-4 px-6">
+
+    <main class="px-4">
       <NuxtPage />
     </main>
+
+    <div
+      v-if="onMobile"
+      class="fixed bottom-0 inset-x-0 p-4"
+    >
+      <!-- TODO: v-if="hasRole('admin')" -->
+      <MobileBar />
+      <!-- TODO:
+      <Button
+        v-else
+        class="w-full"
+        size="lg"
+      >
+        <Plus />
+        Nouvelle simulation
+      </Button>
+      -->
+    </div>
   </div>
 </template>
