d9d812a4b67a2c14dda22335967a8ad15b275bc4
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-07-16 10:27:32 +0200

    :rocket: setup default navigation pages

---
+++ pages/[workspaceId]/team.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const store = useWorkspaceStore();
+const { workspace } = storeToRefs(store);
+</script>
+
+<template>
+  <main data-page="workspace.home">
+    TEAM {{ workspace?.name }}
+  </main>
+</template>
